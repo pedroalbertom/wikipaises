5f4aa2031de4d8416056ba5918d1dc34b6d446b5
Pedro Alberto <pedroalbertompontes@gmail.com>
2026-04-16 12:49:32 -0300

    add home and detail pages

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,7 @@
+export default function Detail() {
+    return (
+        <main>
+            <h2>Detalhes do País</h2>
+        </main>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main>
+            <h2>Explore the Atlas</h2>
+        </main>
+    )
+}
