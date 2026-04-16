2d2d85e708cdaf8ea6715709051b964820f5256f
Pedro Alberto <pedroalbertompontes@gmail.com>
2026-04-16 12:49:46 -0300

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Detail from './pages/Detail'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>
-      <h1>WikiPaises</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/country/:code" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
