571c522a996d557d03717df8a9bf37cebdd7264c
Pedro Alberto <pedroalbertompontes@gmail.com>
2026-04-16 12:52:48 -0300

    add the api services

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,20 @@ const api = axios.create({
     baseURL: 'https://restcountries.com/v3.1'
 })
 
+export const getAllCountries = () => {
+    return api.get('/all');
+};
+
+export const getCountriesByName = (name) => {
+    return api.get(`/name/${name}`);
+};
+
+export const getCountriesByRegion = (region) => {
+    return api.get(`/region/${region}`);
+};
+
+export const getCountryByCode = (code) => {
+    return api.get(`/alpha/${code}`);
+};
+
 export default api
